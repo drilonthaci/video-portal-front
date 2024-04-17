62c31cd1d8a3fa8b888d1516b99b59c2b45d0457
Drilon Thaqi <ddrilonthaci@gmail.com>
2024-04-17 13:52:47 +0200

    Adjusted layout and styling for CategoryDetails

--- src/Screens/Category/CategoryDetails.js
+++ src/Screens/Category/CategoryDetails.js
@@ -1,6 +1,6 @@
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
 
 function CategoryDetails() {
   const { categoryName } = useParams();
@@ -24,25 +24,27 @@ function CategoryDetails() {
     fetchCategory();
   }, [categoryName]);
 
-  if (!category) {
-    return <div>Loading...</div>; 
-  }
-    
-
   return (
-    <div className="container mx-auto py-8">
-       <div className="bg-cover bg-center h-64 flex items-center justify-center" style={{ backgroundImage: `url(${category.imageUrl})` }}>
-          <div className="text-white">
-          <h1 className="text-4xl font-semibold">{category.name}</h1>
-          <p className="text-lg">{category.shortDescription}</p>
+    <div className="bg-gray-100 min-h-screen">
+      <div className="bg-cover bg-center h-96 flex items-center" style={{ backgroundImage: `url(${category?.imageUrl})` }}>
+        <div className="text-white mx-auto w-4/5"> 
+          {category ? (
+            <>
+              <div className="w-3/5">
+                <h1 className="text-4xl font-semibold">{category.name}</h1>
+                <p className="text-balance text-lg mt-2">{category.shortDescription}</p>
+              </div>
+            </>
+          ) : (
+            <p className="text-lg mt-2">Loading category...</p>
+          )}
         </div>
       </div>
-      
-      <div className="mt-8">
-        <h3 className="text-2xl font-semibold mb-4">Contents</h3>
+
+      <div className="container mx-auto py-8">
+        <h3 className="text-2xl font-semibold mb-4 tracking-wide">Contents</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* Render video cards here if category.videos is defined */}
-          {category.videos && category.videos.map(video => (
+          {category?.videos && category.videos.map(video => (
             <div key={video.id} className="bg-white rounded-lg shadow-md overflow-hidden">
               <div className="aspect-w-16 aspect-h-9">
                 <iframe
