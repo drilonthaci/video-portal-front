c6d94f72c941bc0c634ca4829d9113ed823c692d
AlbioneRrudhani <102328598+AlbioneRrudhani@users.noreply.github.com>
2024-04-21 22:24:39 +0200

    Add user authentication to route

--- src/Layout/Routes/RoutesContainer.js
+++ src/Layout/Routes/RoutesContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import NotFound from "../../Screens/NotFound";
 import { CategoryManagement } from "../../Screens/Dashboard/Admin/Category/CategoryManagement";
 import CategoryList from "../../Screens/Category/CategoryList";
@@ -8,23 +8,32 @@ import VideoPostDetails from "../../Screens/VideoPost/VideoPostDetails";
 import VideoPostsList from "../../Screens/Dashboard/Admin/VideoPost/VideoPostsList";
 import Login from "../../Screens/Auth/Login/Login";
 import Register from "../../Screens/Auth/Register/Register";
+import authService from "../../Screens/Auth/Login/AuthService"; // Import authService for authentication checks
 
 function RoutesContainer() {
+  const isLoggedIn = authService.isLoggedIn();
+  const user = authService.getUser();
+
   return (
     <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login />} />
+      <Route path="/register" element={isLoggedIn ? <Navigate to="/" /> : <Register />} />
       <Route path="/categories" element={<CategoryList />} />
       <Route path="/category/:categoryId" element={<CategoryDetails />} />
       <Route path="/video/:videoPostId" element={<VideoPostDetails />} />
-      <Route path="/login"  element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
 
-      
-      {/* admin */}
-      <Route path="/admin/categories" element={<CategoryManagement />} />
-      <Route path="/admin/video-posts" element={<VideoPostsList />} />
+      {/* Protected Admin Routes */}
+      {isLoggedIn && user && user.roles.includes('Creator') && (
+        <>
+          <Route path="/admin/categories" element={<CategoryManagement />} />
+          <Route path="/admin/video-posts" element={<VideoPostsList />} />
+        </>
+      )}
+      {/* 404 Not Found Route */}
       <Route path="*" element={<NotFound />} />
-    </Routes >
+    </Routes>
   );
 }
 
-export default RoutesContainer; 
+export default RoutesContainer;
